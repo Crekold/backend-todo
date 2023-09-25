511c098f697b1576083efc26635a8fc86d5354d9
crekold <crekoldalt@gmail.com>
2023-09-25 00:31:43 -0400

    modificaciones del usercontroller

--- src/main/java/com/backend/todo/controller/UserController.java
+++ src/main/java/com/backend/todo/controller/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.backend.todo.repository.UserRepository;
 import com.backend.todo.model.ErrorResponse;
@@ -39,20 +38,27 @@ public class UserController {
         User savedUser = userRepository.save(newUser);
         return ResponseEntity.ok(savedUser);
     }
-
-    
     @PostMapping("/login")
-public String login(@RequestParam String username, @RequestParam String password) {
-    // Busca el usuario en la base de datos
-    User user = userRepository.findByUsername(username);
-
-    // Verifica si el usuario existe y si la contraseña es correcta
-    if (user == null || !user.getPassword().equals(password)) {
-        return "Credenciales inválidas";
+    public ResponseEntity<?> login(@RequestBody User user) {
+        // Realiza las validaciones necesarias teniendo en cuenta que solo tengo dos campos
+        if (user.getUsername() == null || user.getUsername().isEmpty()) {
+            ErrorResponse error = new ErrorResponse("El nombre de usuario es obligatorio", HttpStatus.BAD_REQUEST.value());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        }
+        if (user.getPassword() == null || user.getPassword().isEmpty()) {
+            ErrorResponse error = new ErrorResponse("La contraseña es obligatoria", HttpStatus.BAD_REQUEST.value());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        }
+        User savedUser = userRepository.findByUsername(user.getUsername());
+        if (savedUser == null) {
+            ErrorResponse error = new ErrorResponse("El usuario no existe", HttpStatus.BAD_REQUEST.value());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        }
+        if (!savedUser.getPassword().equals(user.getPassword())) {
+            ErrorResponse error = new ErrorResponse("La contraseña es incorrecta", HttpStatus.BAD_REQUEST.value());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        }
+        return ResponseEntity.ok("inicio de sesion exitoso");
     }
-
-    // Devuelve un mensaje de éxito
-    return "Inicio de sesión exitoso";
-}
 
 }
